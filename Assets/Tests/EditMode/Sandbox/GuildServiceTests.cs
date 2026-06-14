9444f72022066c4f4019eec286494461dcb0b462
port <port@vltk.local>
2026-06-14 14:44:54 +0700

    fix(coordinate): add 'using VLTK.Sandbox;' to test files for host interface visibility

--- Assets/Tests/EditMode/Sandbox/GuildServiceTests.cs
+++ Assets/Tests/EditMode/Sandbox/GuildServiceTests.cs
@@ -9,6 +9,7 @@
 
 using System.Collections.Generic;
 using NUnit.Framework;
+using VLTK.Sandbox;
 
 namespace VLTK.Tests.Sandbox
 {
--- Assets/Tests/EditMode/Sandbox/PlayerLevelServiceTests.cs
+++ Assets/Tests/EditMode/Sandbox/PlayerLevelServiceTests.cs
@@ -6,6 +6,7 @@
 // -----------------------------------------------------------------------------
 
 using NUnit.Framework;
+using VLTK.Sandbox;
 
 namespace VLTK.Tests.Sandbox
 {
